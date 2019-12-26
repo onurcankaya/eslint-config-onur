5cdf7c7414cfa9e67128ac8be418989db62f3b2d
onurcankaya <onur.eren.cankaya@gmail.com>
2019-12-26 13:03:14 +0100

    implemented react support

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 module.exports = {
+  parser: 'babel-eslint',
   extends: [
     'standard',
     'prettier',
+    'prettier/react',
     'prettier/standard'
   ],
   plugins: [
+    'react',
     'prettier'
   ],
   env: {
     browser: true,
+    es6: true,
     node: true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      spread: true,
+      defaultParams: true
+    }
   },
   rules: {
     'prettier/prettier': [
