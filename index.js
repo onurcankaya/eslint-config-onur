4966f85a137be00a849735d1763a3a2b3990d760
onurcankaya <onur.eren.cankaya@gmail.com>
2019-12-26 10:06:27 +0100

    moved prettier config to index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 module.exports = {
-  extends: 'standard',
+  extends: [
+    'standard',
+    'prettier',
+    'prettier/standard'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        printWidth: 100,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        useTabs: false
+      }
+    ]
+  }
 }
